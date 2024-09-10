01ac3dda76fa66f28acf7889ad2f52d1cdcd0834
AnaChiaramonte <anasenai.lopes@gmail.com>
2024-09-10 14:19:51 -0300

    Atividade dos Fornecedores

--- AppFluxoDeCaixa/Migrations/20240910133646_Test.Designer.cs
+++ AppFluxoDeCaixa/Migrations/20240910133646_Test.Designer.cs
@@ -4,6 +4,7 @@ using AppFluxoDeCaixa.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AppFluxoDeCaixa.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240910133646_Test")]
+    partial class Test
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -129,7 +132,7 @@ namespace AppFluxoDeCaixa.Migrations
 
                     b.HasKey("FornecedorID");
 
-                    b.ToTable("Fornecedores", (string)null);
+                    b.ToTable("Fornecedor", (string)null);
                 });
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityRole", b =>
---
+++ AppFluxoDeCaixa/Migrations/20240910135354_Fornecedores.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppFluxoDeCaixa.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fornecedores : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Fornecedor",
+                table: "Fornecedor");
+
+            migrationBuilder.RenameTable(
+                name: "Fornecedor",
+                newName: "Fornecedores");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Fornecedores",
+                table: "Fornecedores",
+                column: "FornecedorID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Fornecedores",
+                table: "Fornecedores");
+
+            migrationBuilder.RenameTable(
+                name: "Fornecedores",
+                newName: "Fornecedor");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Fornecedor",
+                table: "Fornecedor",
+                column: "FornecedorID");
+        }
+    }
+}
